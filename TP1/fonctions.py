7ff96d245ddcb42925dbda9c085cc5160fd1fd16
fi26-tanjo <tanjona.randrianarisolo@univ-rouen.fr>
2026-09-09 15:54:42 +0200

    ajout des fichiers python

---
+++ TP1/fonctions.py
@@ -0,0 +1,15 @@
+def puissance(a, b):
+#	a = input ("choisissez un premier argument:")
+#	a = float (a)
+
+#	b = input ("choissisez un deuxième argument:")
+#	b = float(b)
+	if not type(a,b) is int:
+		raise TypeError("Only integers are allowed")
+
+	result = a**b
+
+	return result
+#	print(f"le resultat est {result}")
+
+#puissance(a, b)
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,20 @@
-print("Hello, World!")
+import fonctions as f
+
+def carre():
+	while True:
+#		x = input("saisissez un nombre sur votre clavier:")
+#		x = float(x)
+#		result1 = x ** 2
+#		print (f"le resultat est {result}")
+		a = input ("choisissez un premier argument:")
+		a = int (a)
+
+		b = input ("choissisez un deuxième argument:")
+		b = int (b)
+
+		res = f.puissance(a, b)
+
+		print (f"le résultat est {res}")
+
+carre()	
+	
