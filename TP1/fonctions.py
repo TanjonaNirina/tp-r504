745beebf323e3376e9a5c3699069acc7291bd324
fi26-tanjo <tanjona.randrianarisolo@univ-rouen.fr>
2026-09-16 15:35:56 +0200

    modifications version pytest

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,15 +1,20 @@
 def puissance(a, b):
-#	a = input ("choisissez un premier argument:")
-#	a = float (a)
+    if not type(a) is int:
+        raise TypeError("Only integers are allowed")
+    if not type(b) is int:
+        raise TypeError("Only integers are allowed")
+    if a == 0 and b <= 0:
+        raise ValueError("l'exposant doit être strictement positif")
 
-#	b = input ("choissisez un deuxième argument:")
-#	b = float(b)
-	if not type(a) is int:
-		raise TypeError("Only integers are allowed")
-	if not type(b) is int:
-		raise TypeError("Only integers are allowed")
-	result = a**b
-	return result	
-#	print(f"le resultat est {result}")
+    result = 1
+    # iteration de la valeur absolue de l'exposant
+    nb_iterations = -b if b < 0 else b
 
-#puissance(a, b)
+    for _ in range(nb_iterations):
+        result *= a
+
+    # si exposant négatif, on prend l'inverse
+    if b < 0:
+        result = 1 / result
+
+    return result
--- TP1/test.py
+++ TP1/test.py
@@ -11,3 +11,16 @@ def test_2():
 	assert f.puissance(-1, -1) == -1
 	assert f.puissance(-1,-2) == 1
 	assert f.puissance(-2,-1) == -0.5
+
+def test_3():
+	assert f.puissance(0, 5) == 0
+
+	try:
+		f.puissance(0, -1)
+		pytest.fail("L'exception attendue n'a pas été levée")
+	except (ValueError, Exception):
+		pass
+
+
+ 
+
